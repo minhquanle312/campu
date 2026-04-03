28cfad90a796de8c25fcec8fb5898e438740e55e
Minh Quân Lê <minhquanle312@gmail.com>
2026-04-04 01:38:30 +0700

    fix: update type ui for badge

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -31,7 +31,7 @@ function Badge({
   variant = 'default',
   asChild = false,
   ...props
-}: React.ComponentProps<'span'> &
+}: React.ComponentPropsWithoutRef<'span'> &
   VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
   const Comp = asChild ? Slot.Root : 'span'
 
