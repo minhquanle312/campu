52280abc4911865a306cece62db900255d0893de
Minh Quân Lê <minhquanle312@gmail.com>
2026-04-04 01:15:47 +0700

    update turbopack and remove deprecated radix-ui lib

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
+import path from 'path'
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://ik.imagekit.io/campu/**')],
+  },
+  turbopack: {
+    root: path.join(__dirname, '..'),
   },
 }
 
